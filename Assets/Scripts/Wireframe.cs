cad6c482a9951cde7b682d689945bc1b128bdaae
mo49 <k.mochizuki.0409@gmail.com>
2019-05-24 18:03:59 +0900

    first commit

---
+++ Assets/Scripts/Wireframe.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wireframe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshFilter mf = GetComponent<MeshFilter>();
+        mf.mesh.SetIndices(mf.mesh.GetIndices(0), MeshTopology.Lines, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/WireframeForSkin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireframeForSkin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SkinnedMeshRenderer smr = GetComponent<SkinnedMeshRenderer>();
+        smr.sharedMesh.SetIndices(smr.sharedMesh.GetIndices(0), MeshTopology.LineStrip, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
